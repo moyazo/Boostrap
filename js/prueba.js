641f79799de8cf7bf21fd7f0d9e3b8414271206a
DAW <DAW@ILERNA.SEVILLA>
2022-11-14 14:21:52 +0100

    Holatio

--- js/prueba.js
+++ js/prueba.js
@@ -4,20 +4,20 @@
 
 
 
-let idTimeInterval = setInterval(() => {
-    let ceniza = document.createElement("div"); 
-    ceniza.setAttribute("id",idTimeInterval)
-    ceniza.style.background = "lightblue";
-    ceniza.style.position = "absolute";
-    ceniza.style.top = (Math.random() * 100 )+ "%";
-    ceniza.style.right = (Math.random() * 100 )+ "%";
-    ceniza.style.boxShadow = "1px 1px 7px 10px lightblue";
-    ceniza.style.zIndex = -1;
-    ceniza.style.opacity = Math.random();
-    document.body.appendChild(ceniza);
+// let idTimeInterval = setInterval(() => {
+//     let ceniza = document.createElement("div"); 
+    
+//     ceniza.style.background = "lightblue";
+//     ceniza.style.position = "absolute";
+//     ceniza.style.top = (Math.random() * 100 )+ "%";
+//     ceniza.style.right = (Math.random() * 100 )+ "%";
+//     ceniza.style.boxShadow = "1px 1px 7px 10px lightblue";
+//     ceniza.style.zIndex = -1;
+//     ceniza.style.opacity = Math.random();
+//     document.body.appendChild(ceniza);
         
    
-},1000)
+// },1000)
 
 
 
