8911bfd4d16b704af0847b7fe3f64d005460c251
Manuel Moya <manuel.moyavadillo2@gmail.com>
2022-11-15 23:47:28 +0100

    Falta Evento onClick

--- js/prueba.js
+++ js/prueba.js
@@ -1,22 +1,40 @@
 "use strict"
 
+let aux = 0;
 
+    let idTimeInterval = setInterval(() => {
+        let ceniza = document.createElement("div"); 
 
-    // let idTimeInterval = setInterval(() => {
-    //     let ceniza = document.createElement("div"); 
+        let ash = Math.random() - 0.5;
+
+        if(ash > 0){
+            ceniza.setAttribute("id","ash")
+        }else{
+            ceniza.setAttribute("id","ash1")
+        }
         
-    //     ceniza.style.background = "lightblue";
-    //     ceniza.style.position = "absolute";
-    //     ceniza.style.top = (Math.random() * 100 )+ "%";
-    //     ceniza.style.right = (Math.random() * 100 )+ "%";
-    //     ceniza.style.boxShadow = "1px 1px 7px 10px lightblue";
-    //     ceniza.style.zIndex = -1;
-    //     ceniza.style.opacity = Math.random();
-    //     document.body.append(ceniza);
-            
+        ceniza.style.background = "lightblue";
+        ceniza.style.position = "absolute";
+        ceniza.style.top = (Math.random() * 100 )+ "%";
+        ceniza.style.right = (Math.random() * 100 )+ "%";
+        ceniza.style.boxShadow = "1px 1px 7px 5px lightblue";
+        ceniza.style.zIndex = -1;
+        ceniza.style.opacity = Math.random();
+        
+        document.body.append(ceniza);
+           aux++;
+    console.log(aux)
+    if(aux === 50)
+        clearInterval(idTimeInterval);
     
-    // },500)
+    },500)
 
+
+    function showCard(){
+        document.getElementById()
+    }
+
+   
 
 
 
