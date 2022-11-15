5a4600f60bf5bfd55b93a9fdbb209b8dec72d45a
DAW <DAW@ILERNA.SEVILLA>
2022-11-15 14:19:41 +0100

    Holatio4

--- js/prueba.js
+++ js/prueba.js
@@ -2,22 +2,21 @@
 
 
 
-
-
-// let idTimeInterval = setInterval(() => {
-//     let ceniza = document.createElement("div"); 
-    
-//     ceniza.style.background = "lightblue";
-//     ceniza.style.position = "absolute";
-//     ceniza.style.top = (Math.random() * 100 )+ "%";
-//     ceniza.style.right = (Math.random() * 100 )+ "%";
-//     ceniza.style.boxShadow = "1px 1px 7px 10px lightblue";
-//     ceniza.style.zIndex = -1;
-//     ceniza.style.opacity = Math.random();
-//     document.body.appendChild(ceniza);
+    // let idTimeInterval = setInterval(() => {
+    //     let ceniza = document.createElement("div"); 
         
-   
-// },1000)
+    //     ceniza.style.background = "lightblue";
+    //     ceniza.style.position = "absolute";
+    //     ceniza.style.top = (Math.random() * 100 )+ "%";
+    //     ceniza.style.right = (Math.random() * 100 )+ "%";
+    //     ceniza.style.boxShadow = "1px 1px 7px 10px lightblue";
+    //     ceniza.style.zIndex = -1;
+    //     ceniza.style.opacity = Math.random();
+    //     document.body.append(ceniza);
+            
+    
+    // },500)
+
 
 
 
