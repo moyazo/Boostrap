21f1bb4024d33db92efa5d0657206ebcec2bc2fb
DAW <DAW@ILERNA.SEVILLA>
2022-11-16 13:04:58 +0100

    Terminar Weapons

--- js/prueba.js
+++ js/prueba.js
@@ -1,40 +1,46 @@
 "use strict"
 
-let aux = 0;
+// let aux = 0;
 
-    let idTimeInterval = setInterval(() => {
-        let ceniza = document.createElement("div"); 
+//     let idTimeInterval = setInterval(() => {
+//         let ceniza = document.createElement("div"); 
 
-        let ash = Math.random() - 0.5;
+//         let ash = Math.random() - 0.5;
 
-        if(ash > 0){
-            ceniza.setAttribute("id","ash")
-        }else{
-            ceniza.setAttribute("id","ash1")
-        }
+//         if(ash > 0){
+//             ceniza.setAttribute("id","ash")
+//         }else{
+//             ceniza.setAttribute("id","ash1")
+//         }
         
-        ceniza.style.background = "lightblue";
-        ceniza.style.position = "absolute";
-        ceniza.style.top = (Math.random() * 100 )+ "%";
-        ceniza.style.right = (Math.random() * 100 )+ "%";
-        ceniza.style.boxShadow = "1px 1px 7px 5px lightblue";
-        ceniza.style.zIndex = -1;
-        ceniza.style.opacity = Math.random();
+//         ceniza.style.background = "lightblue";
+//         ceniza.style.position = "absolute";
+//         ceniza.style.top = (Math.random() * 100 )+ "%";
+//         ceniza.style.right = (Math.random() * 100 )+ "%";
+//         ceniza.style.boxShadow = "1px 1px 7px 5px lightblue";
+//         ceniza.style.zIndex = -1;
+//         ceniza.style.opacity = Math.random();
         
-        document.body.append(ceniza);
-           aux++;
-    console.log(aux)
-    if(aux === 50)
-        clearInterval(idTimeInterval);
+//         document.body.append(ceniza);
+//            aux++;
+//     console.log(aux)
+//     if(aux === 100)
+//         clearInterval(idTimeInterval);
     
-    },500)
+//     },500)
 
-
+      
     function showCard(){
-       let carta = document.getElementById("carta");
-      let dios = carta.children().getElementById("tarjeta");
+        let dios = document.getElementById("tarjeta");
 
-      dios.style.visibility = "visible";
+        dios.style.visibility = "hidden";
+        
+     if(dios.style.visibility === "hidden"){
+        dios.style.visibility = "visible"
+     }else if(dios.style.visibility === "visible"){
+        dios.style.visibility = "hidden"
+     }
+      
     }
 
    
